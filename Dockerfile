67293ffc7a3e736dccf4bad269bb122f6a455800
Niels Lerche Sørensen <nielslerches@gmail.com>
2021-01-30 09:51:17 +0100

    successfully parse snowplow event with their scala library

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,10 @@ COPY . .
 WORKDIR /usr/share/logstash
 
 RUN echo 'gem "logstash-filter-snowplow_event_transformer", :path => "./plugins/ekstrabladet/snowplow_event_transformer"' >> Gemfile
+RUN echo 'config.support_escapes: true' >> config/logstash.yml
 
 RUN bin/logstash-plugin install --no-verify
+
+COPY _pipeline/pipeline.conf pipeline/pipeline.conf
+
+CMD [ "logstash", "-f", "pipeline/pipeline.conf" ]
--- lib/logstash/filters/snowplow_event_transformer.rb
+++ lib/logstash/filters/snowplow_event_transformer.rb
@@ -1,4 +1,21 @@
 # encoding: utf-8
+require "java"
+
+require 'jar_dependencies'
+require_jar('com.snowplowanalytics', 'snowplow-scala-analytics-sdk_2.13', '2.1.0')
+require_jar('org.scala-lang', 'scala-library', '2.13.2')
+require_jar('com.snowplowanalytics', 'iglu-core-circe_2.13', '1.0.0')
+require_jar('com.snowplowanalytics', 'iglu-core_2.13', '1.0.0')
+require_jar('io.circe', 'circe-core_2.13', '0.13.0')
+require_jar('io.circe', 'circe-numbers_2.13', '0.13.0')
+require_jar('org.typelevel', 'cats-core_2.13', '2.1.1')
+require_jar('org.typelevel', 'cats-macros_2.13', '2.1.1')
+require_jar('org.typelevel', 'cats-kernel_2.13', '2.1.1')
+require_jar('io.circe', 'circe-parser_2.13', '0.13.0')
+require_jar('io.circe', 'circe-jawn_2.13', '0.13.0')
+require_jar('io.circe', 'circe-generic_2.13', '0.13.0')
+require_jar('com.chuusai', 'shapeless_2.13', '2.3.3')
+
 require "logstash/filters/base"
 require "logstash/namespace"
 
@@ -7,6 +24,7 @@ require "logstash/namespace"
 #
 # It is only intended to be used as an example.
 class LogStash::Filters::SnowplowEventTransformer < LogStash::Filters::Base
+  java_import "com.snowplowanalytics.snowplow.analytics.scalasdk.Event"
 
   # Setting the config_name here is required. This is how you
   # configure this filter from your Logstash config.
@@ -30,19 +48,17 @@ class LogStash::Filters::SnowplowEventTransformer < LogStash::Filters::Base
 
   public
   def filter(event)
+    # Replace the event message with our message as configured in the
+    # config file.
 
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
+    snowplow_event = Event.parse(event.get("message"))
 
-      # using the event.set API
-      event.set("message", @message)
-      # correct debugging log statement for reference
-      # using the event.get API
+    if snowplow_event.is_valid
+      json = snowplow_event.get_or_else('').to_json(true).no_spaces
+      event.set("message", json)
+
       @logger.debug? && @logger.debug("Message is now: #{event.get("message")}")
+      filter_matched(event)
     end
-
-    # filter_matched should go in the last line of our successful code
-    filter_matched(event)
   end # def filter
 end # class LogStash::Filters::Example
